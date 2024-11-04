3dc28e167208d011f1498a44d4a2a4ba3c033b68
Ivan <i.cabellos@id.uib.eu>
2024-11-04 12:40:05 +0100

    added stadistics

--- Core/urls.py
+++ Core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Core.views import IndexView
+from Core.views import IndexView, CollectionCardsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('show-collection/', CollectionCardsView.as_view(), name='view_collection'),
     path('open-booster/', IndexView.as_view(), name='open_booster')
 ]
